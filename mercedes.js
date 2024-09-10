40d5ad7b7bb4819cb79306ab3856c77c07ad810e
mrhoba9 <155851960+mrhoba9@users.noreply.github.com>
2024-09-10 03:23:42 +0300

    Add files via upload

---
+++ mercedes.js
@@ -0,0 +1,386 @@
+/*piston decleration starts*/
+const pistoncardContainer = document.getElementById(
+	"Engine-Components-Pistons"
+);
+const Engine_Components_Crankshafts_Container = document.getElementById(
+	"Engine-Components-Crankshafts"
+);
+const Engine_Components_Cylinder_HeadsContainer = document.getElementById(
+	"Engine-Components-Cylinder-Heads"
+);
+const Engine_Components_Turbochargers_container = document.getElementById(
+	"Engine-Components-Turbochargers"
+);
+const PistonArray = [];
+const CrankshaftsArray = [];
+const Cylinder_HeadsArray = [];
+const TurbochargersArray = [];
+/*piston decleratione ends*/
+
+/*break decleratione starts*/
+const brakePadsContainer = document.getElementById("Brake-Systems-Brake-Pads");
+const brakeRotorsContainer = document.getElementById(
+	"Brake-Systems-Brake-Rotors"
+);
+const brakeCalipersContainer = document.getElementById(
+	"Brake-Systems-Brake-Calipers"
+);
+const brakePadsArray = [];
+const brakeRotorsArray = [];
+const brakeCalipersArray = [];
+/*break decleratione ends*/
+
+/*Electrical Components starts*/
+const alternatorsContainer = document.getElementById(
+	"Electrical-Components-Alternators"
+);
+const startersContainer = document.getElementById(
+	"Electrical-Components-Starters"
+);
+const wiringHarnessesContainer = document.getElementById(
+	"Electrical-Components-Wiring-Harnesses"
+);
+const alternatorsArray = [];
+const startersArray = [];
+const wiringHarnessesArray = [];
+/*Electrical Components ends*/
+
+/*body parts starts*/
+const bumpersContainer = document.getElementById("Body-Parts-Bumpers");
+const mirrorsContainer = document.getElementById("Body-Parts-Mirrors");
+const grillesContainer = document.getElementById("Body-Parts-Grilles");
+const bumpersArray = [];
+const mirrorsArray = [];
+const grillesArray = [];
+/*body parts ends*/
+
+/*Accessories starts*/
+const accessoriesLightsContainer =
+	document.getElementById("Accessories-Lights");
+const airHornsContainer = document.getElementById("Accessories-Air-Horns");
+const floorMatsContainer = document.getElementById("Accessories-Floor-Mats");
+const accessoriesLightsArray = [];
+const airHornsArray = [];
+const floorMatsArray = [];
+/*Accessories ends*/
+
+/*category buttons starts*/
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+const sixth = document.getElementById("sixth");
+const seventh = document.getElementById("seventh");
+/*category buttons ends*/
+
+/*other decleration starts*/
+const SearchNameInp = document.getElementById("SearchName");
+let mode = "Piston";
+const favContainer = document.getElementById("fav-container");
+let favArray = localStorage.getItem("favArrayStorage") ? JSON.parse(localStorage.getItem("favArrayStorage")) : [];
+let remainCardsText = document.getElementById("remainCards");
+/*other decleration ends*/
+
+document.addEventListener("DOMContentLoaded", () => {
+	fetchAndImplement("pistons", PistonArray, pistoncardContainer);
+	fetchAndImplement(
+		"crankshafts",
+		CrankshaftsArray,
+		Engine_Components_Crankshafts_Container
+	);
+	fetchAndImplement(
+		"Cylinder-Heads",
+		Cylinder_HeadsArray,
+		Engine_Components_Cylinder_HeadsContainer
+	);
+	fetchAndImplement(
+		"Turbochargers",
+		TurbochargersArray,
+		Engine_Components_Turbochargers_container
+	);
+});
+
+/*Engine Components starts*/
+function fetchAndImplement(componentName, array, container) {
+	fetch(`.././json/Engine-Components-${componentName}.json`)
+		.then((response) => response.json())
+		.then((data) => {
+			array.length = 0;
+			data.forEach((item) => array.push(item));
+			shuffleArray(array);
+			implementComponent(array, container);
+		})
+
+		.catch((error) =>
+			console.error(`Error fetching ${componentName} JSON data:`, error)
+		);
+}
+
+/*shuffle the arrays starts*/
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}
+/*shuffle the arrays ends */
+
+function implementComponent(array, container) {
+	container.innerHTML = "";
+	array.forEach((item) => (container.innerHTML += cardHTMLTemplate(item)));
+}
+/*Engine Components ends*/
+
+/*breakSystem starts*/
+first.onclick = () => {
+	mode = "Piston";
+	SearchNameInp.value = "";
+	fetchAndImplement("pistons", PistonArray, pistoncardContainer);
+	fetchAndImplement(
+		"crankshafts",
+		CrankshaftsArray,
+		Engine_Components_Crankshafts_Container
+	);
+	fetchAndImplement(
+		"Cylinder-Heads",
+		Cylinder_HeadsArray,
+		Engine_Components_Cylinder_HeadsContainer
+	);
+	fetchAndImplement(
+		"Turbochargers",
+		TurbochargersArray,
+		Engine_Components_Turbochargers_container
+	);
+};
+second.onclick = () => {
+	mode = "Brake Systems";
+	SearchNameInp.value = "";
+	fetchAndImplement("brakePads", brakePadsArray, brakePadsContainer);
+	fetchAndImplement("brakeRotors", brakeRotorsArray, brakeRotorsContainer);
+	fetchAndImplement(
+		"brakeCalipers",
+		brakeCalipersArray,
+		brakeCalipersContainer
+	);
+};
+third.onclick = () => {
+	mode = "Engine Components";
+	SearchNameInp.value = "";
+	fetchAndImplement("alternators", alternatorsArray, alternatorsContainer);
+	fetchAndImplement("starters", startersArray, startersContainer);
+	fetchAndImplement(
+		"wiringHarnesses",
+		wiringHarnessesArray,
+		wiringHarnessesContainer
+	);
+};
+sixth.onclick = () => {
+	mode = "Body Parts";
+	SearchNameInp.value = "";
+	fetchAndImplement("bumpers", bumpersArray, bumpersContainer);
+	fetchAndImplement("mirrors", mirrorsArray, mirrorsContainer);
+	fetchAndImplement("grilles", grillesArray, grillesContainer);
+};
+seventh.onclick = () => {
+	mode = "Accessories";
+	SearchNameInp.value = "";
+	fetchAndImplement(
+		"accessoriesLights",
+		accessoriesLightsArray,
+		accessoriesLightsContainer
+	);
+	fetchAndImplement("airHorns", airHornsArray, airHornsContainer);
+	fetchAndImplement("floorMats", floorMatsArray, floorMatsContainer);
+};
+/*breakSystem ends*/
+
+/*search Starts*/
+function SearchByName() {
+	const inputValue = SearchNameInp.value.toLowerCase();
+	let combinedArray = [];
+	let htmlStrings = {};
+	let containers = {};
+
+	if (mode === "Piston") {
+		combinedArray = [
+			...PistonArray,
+			...CrankshaftsArray,
+			...Cylinder_HeadsArray,
+			...TurbochargersArray,
+		];
+		containers = {
+			piston: pistoncardContainer,
+			crankshaft: Engine_Components_Crankshafts_Container,
+			CylinderHead: Engine_Components_Cylinder_HeadsContainer,
+			turboChangers: Engine_Components_Turbochargers_container,
+		};
+
+		htmlStrings = {
+			piston: "",
+			crankshaft: "",
+			CylinderHead: "",
+			turboChangers: "",
+		};
+	} else if (mode === "Brake Systems") {
+		combinedArray = [
+			...brakePadsArray,
+			...brakeRotorsArray,
+			...brakeCalipersArray,
+		];
+		containers = {
+			brakePads: brakePadsContainer,
+			brakeRotors: brakeRotorsContainer,
+			brakeCalipers: brakeCalipersContainer,
+		};
+		htmlStrings = {
+			brakePads: "",
+			brakeRotors: "",
+			brakeCalipers: "",
+		};
+	} else if (mode == "Engine Components") {
+		combinedArray = [
+			...alternatorsArray,
+			...startersArray,
+			...wiringHarnessesArray,
+		];
+		console.log(combinedArray);
+		containers = {
+			alternator: alternatorsContainer,
+			starter: startersContainer,
+			wiringHarness: wiringHarnessesContainer,
+		};
+		htmlStrings = {
+			alternator: "",
+			starter: "",
+			wiringHarness: "",
+		};
+	} else if (mode == "Body Parts") {
+		combinedArray = [...bumpersArray, ...mirrorsArray, ...grillesArray];
+		console.log(combinedArray);
+		containers = {
+			bumpers: bumpersContainer,
+			mirrors: mirrorsContainer,
+			grilles: grillesContainer,
+		};
+		htmlStrings = {
+			bumpers: "",
+			mirrors: "",
+			grilles: "",
+		};
+	} else if (mode == "Accessories") {
+		combinedArray = [
+			...accessoriesLightsArray,
+			...airHornsArray,
+			...floorMatsArray,
+		];
+		console.log(combinedArray);
+		containers = {
+			accessoriesLights: accessoriesLightsContainer,
+			airHorns: airHornsContainer,
+			floorMats: floorMatsContainer,
+		};
+		htmlStrings = {
+			accessoriesLights: "",
+			airHorns: "",
+			floorMats: "",
+		};
+	}
+
+	combinedArray.forEach((item) => {
+		if (item.name.toLowerCase().includes(inputValue)) {
+			if (containers[item.type]) {
+				htmlStrings[item.type] += cardHTMLTemplate(item);
+			}
+		}
+	});
+	Object.keys(containers).forEach((type) => {
+		containers[type].innerHTML = htmlStrings[type] || "No Data Found";
+	});
+}
+/*search ends*/
+SearchNameInp.addEventListener("keyup", () => {
+	SearchByName();
+	if (event.key === "Enter") {
+		SearchByName();
+	}
+});
+/*implements starts*/
+function cardHTMLTemplate(item) {
+	return `
+    <div class="card" id="Pistons-card-${item.type + item.id}">
+        <img src="${item.image}" loading="lazy" alt="${item.name}"/>
+        <div class="card-body">
+            <div class="row">
+                <div class="card-title">
+                    <h4>${item.name}</h4>
+                    <h4>${item.size}</h4>
+                    <h3>${item.price}</h3>
+                </div>
+                <div class="view-btn">
+					<i id="${item.type + item.id}" onclick='favContainerFunc(this.id, ${JSON.stringify(item)})' class="material-symbols-outlined">shopping_cart</i>
+				</div>
+            </div>
+            <hr />
+        </div>
+    </div>
+    `;
+}
+function initializeFavorites() {
+	
+    favArray.forEach(item => {
+        favContainer.innerHTML += favImplement(item);
+    });
+	if(favArray !== ""){
+		remainCards.innerText = `${favArray.length}/40`;
+	}
+}
+initializeFavorites()
+
+function favContainerFunc(id,item){
+	let exists = false;
+	for(let i = 0; i < favArray.length; i++) {
+		if(id === favArray[i].type + favArray[i].id) {
+			exists = true;
+			break;
+		}
+	}
+	if (!exists && favArray.length <= 39) {
+		favArray.push(item);
+		localStorage.setItem("favArrayStorage", JSON.stringify(favArray));
+		console.log(favArray.length)
+		favContainer.innerHTML += favImplement(item);
+		remainCards.innerText = `${favArray.length}/40`;
+		localStorage.setItem("remainCardsStorage", JSON.stringify(remainCards.innerText));
+	}
+}
+function favImplement(item){
+	return `
+		<div class="Fav-list-item p-3 relative">
+			<img src="${item.image}" alt="${item.image}" class="Fav-Image" id="searchImageResult">
+			<p class="FavText">${item.name}</p>
+			<p class="FavPrice text-red-600 font-bold">${item.price}.00</p>
+			<i id="${item.type + item.id}" onclick='removeFav(this.id, ${JSON.stringify(item)})' class="material-symbols-outlined absolute right-1 top-1 cursor-pointer text-black font-bold select-none">close</i>
+		</div>
+	`
+}
+
+function removeFav(id){
+	const index = favArray.findIndex(fav => fav.type + fav.id === id);
+    if (index !== -1) {
+        favArray.splice(index, 1);
+		localStorage.setItem("favArrayStorage", JSON.stringify(favArray));
+		remainCards.innerText = `${favArray.length}/40`;
+		localStorage.setItem("remainCardsStorage", JSON.stringify(remainCards.innerText));
+		console.log(favArray.length);
+		implementAfterRemove();
+    } else {
+        console.log("Item not found.");
+    }
+}
+function implementAfterRemove(){
+	console.log("implementAfterRemove"+ favArray.length);
+	favContainer.innerHTML = "";
+	favArray.forEach(index =>{
+		favContainer.innerHTML += favImplement(index);
+	})
+	
+}
---
+++ script.js
@@ -0,0 +1,142 @@
+/*menu buttons starts*/
+document.addEventListener("DOMContentLoaded", function () {
+	const menuButton = document.querySelector(
+		'[data-collapse-toggle="navbar-default"]'
+	);
+	const menu = document.getElementById("navbar-default");
+
+	menuButton.addEventListener("click", function () {
+		menu.classList.toggle("hidden");
+	});
+});
+/*menu buttons ends*/
+
+/*popup div starts*/
+const engineComponentsSection = document.getElementById('Engine-Components');
+const brakeSystemsSection = document.getElementById('Brake-Systems');
+const electricalComponentsSection = document.getElementById('Electrical-Components');
+const bodyPartsSection = document.getElementById('Body-Parts');
+const accessoriesSection = document.getElementById('Accessories');
+let pop_div_holder = document.getElementById("pop_div_holder");
+category_search.onclick = () => {
+	pop_div_holder.classList.remove("hide");
+	pop_div_holder.classList.add("show-block");
+};
+first.addEventListener("click", () => {
+	engineComponentsSection.classList.remove("hide");
+	brakeSystemsSection.classList.add("hide");
+	electricalComponentsSection.classList.add("hide");
+	bodyPartsSection.classList.add("hide");
+	accessoriesSection.classList.add("hide");
+	scroller();
+	pop_div_holder.classList.add("hide");
+	first.style.backgroundColor = "#ffffff23";
+	second.style.backgroundColor = "transparent";
+	third.style.backgroundColor = "transparent";
+	sixth.style.backgroundColor = "transparent";
+	seventh.style.backgroundColor = "transparent";
+});
+second.addEventListener("click", () => {
+	brakeSystemsSection.classList.remove("hide");
+	engineComponentsSection.classList.add("hide");
+	electricalComponentsSection.classList.add("hide");
+	bodyPartsSection.classList.add("hide");
+	accessoriesSection.classList.add("hide");
+	scroller();
+	pop_div_holder.classList.add("hide");
+	first.style.backgroundColor = "transparent";
+	second.style.backgroundColor = "#ffffff23";
+	third.style.backgroundColor = "transparent";
+	sixth.style.backgroundColor = "transparent";
+	seventh.style.backgroundColor = "transparent";
+});
+third.addEventListener("click", () => {
+	electricalComponentsSection.classList.remove("hide");
+	engineComponentsSection.classList.add("hide");
+	brakeSystemsSection.classList.add("hide");
+	bodyPartsSection.classList.add("hide");
+	accessoriesSection.classList.add("hide");
+	scroller();
+	pop_div_holder.classList.add("hide");
+	first.style.backgroundColor = "transparent";
+	second.style.backgroundColor = "transparent";
+	third.style.backgroundColor = "#ffffff23";
+	sixth.style.backgroundColor = "transparent";
+	seventh.style.backgroundColor = "transparent";
+});
+sixth.addEventListener("click", () => {
+	bodyPartsSection.classList.remove("hide");
+	engineComponentsSection.classList.add("hide");
+	brakeSystemsSection.classList.add("hide");
+	electricalComponentsSection.classList.add("hide");
+	accessoriesSection.classList.add("hide");
+	scroller();
+	pop_div_holder.classList.add("hide");
+	first.style.backgroundColor = "transparent";
+	second.style.backgroundColor = "transparent";
+	third.style.backgroundColor = "transparent";
+	sixth.style.backgroundColor = "#ffffff23";
+	seventh.style.backgroundColor = "transparent";
+});
+seventh.addEventListener("click", () => {
+	accessoriesSection.classList.remove("hide");
+	engineComponentsSection.classList.add("hide");
+	brakeSystemsSection.classList.add("hide");
+	electricalComponentsSection.classList.add("hide");
+	bodyPartsSection.classList.add("hide");
+	scroller();
+	pop_div_holder.classList.add("hide");
+	first.style.backgroundColor = "transparent";
+	second.style.backgroundColor = "transparent";
+	third.style.backgroundColor = "transparent";
+	sixth.style.backgroundColor = "transparent";
+	seventh.style.backgroundColor = "#ffffff23";
+});
+function scroller(){
+	window.scrollTo(0,0);
+}
+/*popup div ends*/
+
+
+/*arrow starts */
+const arrow_btn = document.getElementById("arrow-up-btn");
+function arrow_func_scroll() {
+	if (window.scrollY >= 940) {
+		arrow_btn.style.opacity = "1";
+		arrow_btn.style.zIndex = "20";
+	} else {
+		arrow_btn.style.opacity = "0";
+		arrow_btn.style.zIndex = "-1";
+	}
+}
+arrow_func_scroll();
+window.addEventListener("scroll", arrow_func_scroll);
+function scrollToTop() {
+	window.scrollTo({ top: 0, behavior: "smooth" });
+};
+window.onload = () => {
+	scrollToTop();
+	localStorage.clear()
+};
+arrow_btn.addEventListener("click", scrollToTop);
+/*arrow ends */
+
+/*aside starts*/
+const closeAside = document.getElementById("close_aside_fav");
+const aside_conatiner = document.getElementById("aside-conatiner");
+const view_aside_container = document.getElementById("view_aside_container");
+closeAside.addEventListener("click",()=>{
+	aside_conatiner.style.padding = "0";
+	aside_conatiner.style.width = "0";
+	setTimeout(()=>{
+		aside_conatiner.style.display = "none";
+	},300);
+})
+view_aside_container.addEventListener("click",()=>{
+	aside_conatiner.style.display = "flex";
+	setTimeout(()=>{
+		aside_conatiner.style.padding = "40px";
+		aside_conatiner.style.width = "450px";
+	},200);
+})
+/*aside endss*/
